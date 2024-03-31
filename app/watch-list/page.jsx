5061f16f0612ba07bc015c2de2e87e4079f9d648
MishSoft <michaelmakaveli123@gmail.com>
2024-03-31 20:52:14 +0400

    add watch list component,  where u can delete and edit list

--- app/watch-list/page.jsx
+++ app/watch-list/page.jsx
@@ -1,17 +1,67 @@
-export default function WatchList() {
+import { cookies } from "next/headers";
+import EditWatch from "../components/EditWatch";
+import WatchForm from "../components/WatchForm";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { deleteWatch } from "../server-actions/deleteWatch";
+
+export default async function WatchList() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
+
+  const { data: watches, error } = await supabase
+    .from("watches")
+    .select("*")
+    .eq("user_id", user.id)
+    .order("brand", { ascending: true });
+
+  if (error) {
+    console.error("Error fetching watches");
+  }
+
   return (
-    <div>
-      <div>
-        <div>
-          <h1>My Watch List</h1>
+    <div className="min-h-screen bg-gray-900 text-gray-300">
+      <div className="container mx-auto p-6 sm:p-12">
+        <div className="flex justify-between items-start">
+          <h1 className="text-5xl md:text-6xl font-extrabold text-white mb-6">
+            My Watch List
+          </h1>
           <form action="/auth/signout" method="post">
             <button
-              className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
               type="submit"
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
             >
-              Sign Out
+              Sign out
             </button>
           </form>
+        </div>
+        <WatchForm />
+        <div className="mt-6">
+          {watches.map((watch) => (
+            <div
+              key={watch.id}
+              className="mb-4 p-4 bg-gray-800 rounded-lg shadow"
+            >
+              <h2 className="text-xl text-white mb-2">
+                {watch.brand} - {watch.model}
+              </h2>
+              <div className="flex space-x-2">
+                <form action={deleteWatch}>
+                  <input type="hidden" name="id" value={watch.id} />
+                  <button
+                    type="submit"
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
+                </form>
+                <EditWatch watch={watch} />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
